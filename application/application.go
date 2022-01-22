9008b7626050f52ac714e7d263dff8f86a725f26
Monika Palova <moni99nort@outlook.com>
2022-01-22 11:58:57 +0200

    setup app

--- application/application.go
+++ application/application.go
@@ -1,11 +1,34 @@
 package application
 
-type Application struct {
+import (
+	"net/http"
 
+	"github.com/MonikaPalova/currency-master/handlers"
+	"github.com/gorilla/mux"
+)
+
+type Application struct {
+	router *mux.Router
 }
 
-function New() Application {
+func New() Application {
 	var a Application
+	a.setupHTTP()
 	// setup app
 	return a
+}
+
+func (a *Application) Start() error {
+	return http.ListenAndServe(":7777", a.router)
+}
+
+func (a *Application) setupHTTP() {
+	a.router = mux.NewRouter()
+
+	a.setupAssetsHandler()
+}
+
+func (a *Application) setupAssetsHandler() {
+	assetsHandler := handlers.NewAssetsHandler()
+	a.router.Path("/assets").Methods(http.MethodGet).HandlerFunc(assetsHandler.Get)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
 
-func main(){
+	"github.com/MonikaPalova/currency-master/application"
+)
+
+func main() {
 	app := application.New()
-	if err=:=app.Start(); err!=nil {
+	if err := app.Start(); err != nil {
+		log.Fatalf("An error occured when starting the aplication: %s", err.Error())
 	}
 }
