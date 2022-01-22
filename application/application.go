d7968805c79df3b30b11fc2982da5d185fb07ca8
Monika Palova <moni99nort@outlook.com>
2022-01-22 23:00:42 +0200

    add mysql to project

--- application/application.go
+++ application/application.go
@@ -1,21 +1,40 @@
 package application
 
 import (
+	"log"
 	"net/http"
 
+	. "github.com/MonikaPalova/currency-master/db"
 	"github.com/MonikaPalova/currency-master/handlers"
 	"github.com/gorilla/mux"
 )
 
+const (
+	user     = "root"
+	password = ""
+	dbname   = "currency-master"
+)
+
 type Application struct {
+	db     *Database
 	router *mux.Router
 }
 
 func New() Application {
 	var a Application
+	a.initDB()
 	a.setupHTTP()
 	// setup app
 	return a
+}
+
+func (a *Application) initDB() {
+	var err error
+	a.db, err = NewDB(user, password, dbname)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println("Successful database connection!")
 }
 
 func (a *Application) Start() error {
@@ -26,10 +45,18 @@ func (a *Application) setupHTTP() {
 	a.router = mux.NewRouter()
 
 	a.setupAssetsHandler()
+	a.setupUsersHandler()
 }
 
 func (a *Application) setupAssetsHandler() {
 	assetsHandler := handlers.NewAssetsHandler()
-	a.router.Path("/assets").Methods(http.MethodGet).HandlerFunc(assetsHandler.Get)
+	a.router.Path("/assets").Methods(http.MethodGet).HandlerFunc(assetsHandler.GetAll)
 	a.router.Path("/assets/{id}").Methods(http.MethodGet).HandlerFunc(assetsHandler.GetById)
+}
+
+func (a *Application) setupUsersHandler() {
+	usersHandler := handlers.UsersHandler{DB: a.db.UsersDBHandler}
+	a.router.Path("/users").Methods(http.MethodGet).HandlerFunc(usersHandler.GetAll)
+	a.router.Path("/users/{username}").Methods(http.MethodGet).HandlerFunc(usersHandler.GetByUsername)
+	a.router.Path("/users").Methods(http.MethodPost).HandlerFunc(usersHandler.Post)
 }
---
+++ db/database.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	createTablesFile = "./sql/create_tables.sql"
+)
+
+type Database struct {
+	conn *sql.DB
+
+	UsersDBHandler *UsersDBHandler
+}
+
+func NewDB(user, password, dbname string) (*Database, error) {
+	dbConnStr := fmt.Sprintf("%s:%s@/%s?multiStatements=true", user, password, dbname)
+	conn, err := sql.Open("mysql", dbConnStr)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to database: %v", err)
+	}
+
+	createTablesQuery, err := ioutil.ReadFile(createTablesFile)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't read file with create tables queries: %v", err)
+	}
+
+	if _, err := conn.Exec(string(createTablesQuery)); err != nil {
+		return nil, fmt.Errorf("request to create tables in db failed: %v", err)
+	}
+
+	return &Database{conn: conn, UsersDBHandler: &UsersDBHandler{conn: conn}}, nil
+}
