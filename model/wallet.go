5aaf410822695896bee39c4ad8f4a6d0a426fb4c
MonikaPalova <moni99nort@outlook.com>
2022-02-09 14:12:30 +0200

    add name to user asset

--- model/wallet.go
+++ model/wallet.go
@@ -5,6 +5,7 @@ import "time"
 type UserAsset struct {
 	username string
 	assetId  string
+	name string
 	quantity uint8
 }
 
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,29 +5,29 @@ USE `currency-master`;
 CREATE TABLE IF NOT EXISTS `USERS` (
     `username` VARCHAR(36) NOT NULL PRIMARY KEY,
     `password` VARCHAR(36) NOT NULL,
-    `email` VARCHAR(64) NOT NULL,
-    `phone_number` VARCHAR(15) NOT NULL
+    `email` VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `USER_ASSETS` (
     `username` VARCHAR(36) NOT NULL,
-    `asset_id` VARCHAR(36) NOT NULL,
+    `asset_id` VARCHAR(10) NOT NULL,
+    `name` VARCHAR(36) NOT NULL,
     `quantity` FLOAT NOT NULL,
     FOREIGN KEY (username) REFERENCES USERS(username)
 );
 
 CREATE TABLE IF NOT EXISTS `ACQUISITIONS` (
     `username` VARCHAR(36) NOT NULL,
-    `asset_id` VARCHAR(36) NOT NULL,
+    `asset_id` VARCHAR(10) NOT NULL,
     `quantity` FLOAT NOT NULL,
     `price_usd` FLOAT NOT NULL,
     `created` DATETIME NOT NULL,
     FOREIGN KEY (username) REFERENCES USERS(username)
 );
 
-CREATE TABLE IF NOT EXISTS `SESSIONS` (
-    `id` VARCHAR(128) NOT NULL PRIMARY KEY,
-    `owner` VARCHAR(36) NOT NULL,
-    `expiration` DATETIME NOT NULL,
-    FOREIGN KEY (owner) REFERENCES USERS(username)
-);
+-- CREATE TABLE IF NOT EXISTS `SESSIONS` (
+--     `id` VARCHAR(128) NOT NULL PRIMARY KEY,
+--     `owner` VARCHAR(36) NOT NULL,
+--     `expiration` DATETIME NOT NULL,
+--     FOREIGN KEY (owner) REFERENCES USERS(username)
+-- );
