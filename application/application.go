1897bda82e9eb0e8544aa6cc5db9ef9b6b18a849
MonikaPalova <moni99nort@outlook.com>
2022-02-09 14:13:06 +0200

    add new user assets paths

--- application/application.go
+++ application/application.go
@@ -13,6 +13,10 @@ const (
 	user     = "root"
 	password = ""
 	dbname   = "currency-master"
+
+	USERS_API_V1       = "/api/v1/users"
+	ASSETS_API_V1      = "/api/v1/assets"
+	USER_ASSETS_API_V1 = "/api/v1/users/{username}/assets"
 )
 
 type Application struct {
@@ -37,7 +41,7 @@ func (a *Application) initDB() {
 	log.Println("Successful database connection!")
 }
 
-func (a *Application) Start() error {
+func (a Application) Start() error {
 	return http.ListenAndServe(":7777", a.router)
 }
 
@@ -46,17 +50,27 @@ func (a *Application) setupHTTP() {
 
 	a.setupAssetsHandler()
 	a.setupUsersHandler()
+	a.setupUserAssetsHandler()
 }
 
 func (a *Application) setupAssetsHandler() {
 	assetsHandler := handlers.NewAssetsHandler()
-	a.router.Path("/assets").Methods(http.MethodGet).HandlerFunc(assetsHandler.GetAll)
-	a.router.Path("/assets/{id}").Methods(http.MethodGet).HandlerFunc(assetsHandler.GetById)
+	a.router.Path(ASSETS_API_V1).Methods(http.MethodGet).HandlerFunc(assetsHandler.GetAll)
+	a.router.Path(ASSETS_API_V1 + "/{id}").Methods(http.MethodGet).HandlerFunc(assetsHandler.GetById)
 }
 
 func (a *Application) setupUsersHandler() {
 	usersHandler := handlers.UsersHandler{DB: a.db.UsersDBHandler}
-	a.router.Path("/users").Methods(http.MethodGet).HandlerFunc(usersHandler.GetAll)
-	a.router.Path("/users/{username}").Methods(http.MethodGet).HandlerFunc(usersHandler.GetByUsername)
-	a.router.Path("/users").Methods(http.MethodPost).HandlerFunc(usersHandler.Post)
+	a.router.Path(USERS_API_V1).Methods(http.MethodGet).HandlerFunc(usersHandler.GetAll)
+	a.router.Path(USERS_API_V1 + "/{username}").Methods(http.MethodGet).HandlerFunc(usersHandler.GetByUsername)
+	a.router.Path(USERS_API_V1).Methods(http.MethodPost).HandlerFunc(usersHandler.Post)
+}
+
+func (a *Application) setupUserAssetsHandler() {
+	userAssetsHandler := handlers.UserAssetsHanlder(a.db.UserAssetsDBHandler)
+
+	a.router.Path(USER_ASSETS_API_V1).Methods(http.MethodGet).HandlerFunc(userAssetsHandler.GetAll)
+	a.router.Path(USER_ASSETS_API_V1 + "/{id}").Methods(http.MethodGet).HandlerFunc(userAssetsHandler.GetByID)
+	a.router.Path(USER_ASSETS_API_V1 + "/{id}/buy").Methods(http.MethodPost).HandlerFunc(userAssetsHandler.Buy)
+	a.router.Path(USER_ASSETS_API_V1 + "/{id}/sell").Methods(http.MethodPost).HandlerFunc(userAssetsHandler.Sell)
 }
